f173cdd567ac0f870fc40be69844ad39a5301193
Lungile Mawila <lungilemawila5@gmail.com>
2024-12-29 18:48:40 +0200

    introduction to globals

---
+++ globals.js
@@ -0,0 +1,16 @@
+// Understanding how globals works in preparation of Modules 
+
+console.log(module)
+
+// checking the filename 
+
+console.log(__filename)
+
+// set Interval takes in a fall back function that will execute once per interval, eg, once per second in the example below
+setInterval(() => {
+    console.log("Hello World, happy you learning node.js with me!")
+}, 1000)
+
+
+// ohh are you new to the terminal? press ctrl + C to terminate the indefinite looping!
+
