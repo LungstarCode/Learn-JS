b8a26e36e98106fae081551f38853acf11cc93be
Lungile Mawila <lungilemawila5@gmail.com>
2024-12-29 19:16:21 +0200

    added variables for game setting controls

--- pure_js_fcc/dragon fight game/script.js
+++ pure_js_fcc/dragon fight game/script.js
@@ -1,4 +1,14 @@
 
+// craeting some variables for the game, must use let so they can be latter changed
+
+let xp = 0;
+let gold = 0;
+let health = 100;
+let currenWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
 
 // getting the objects for all  the button in the html
 const button1 = document.querySelector("#button1")
