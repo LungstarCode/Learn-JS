e8a32d45242a2cd823267258ddd583f7199796ca
Lungile Mawila <lungilemawila5@gmail.com>
2024-12-31 20:05:19 +0200

    new updates

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -1,1 +1,12 @@
 // scripts
+
+// variables to control form validation 
+
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output')
+
+
